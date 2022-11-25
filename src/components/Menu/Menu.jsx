f2b6efbc65d592af93302886a32f21cb4d96bc19
Alex Lopez <coquixo98@gmail.com>
2022-11-25 20:12:25 +0100

    menu created

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,8 +1,31 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Menu.scss";
 
+
+
+
 const Menu = () => {
-  return <div className="menuDesign"></div>;
+  //hooks
+
+  const navigate = useNavigate();
+
+
+
+  const onClick = (e) => {
+    e.preventDefault();
+    navigate("/login");
+  };
+
+
+  return (
+    <div className="menuDesign">
+      <div className="menuButtonDesign" onClick={onClick}>Rented Movies</div>
+      <div className="menuButtonDesign">Rented Series</div>
+      <div className="menuButtonDesign">User Settings</div>
+      <div className="menuButtonDesign logOutDesign">Log Out</div>
+    </div>
+  );
 };
 
 export default Menu;
