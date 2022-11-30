4b11d5aa55f18a1d861029b1c8986fb9cc7d5092
Alex Lopez <coquixo98@gmail.com>
2022-11-30 21:01:56 +0100

    cleaning code on api calls

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -1,50 +1,51 @@
-import axios from 'axios';
+import axios from "axios";
+import { movieApi } from "./movieApi";
 
 const dataBase = "http://localhost:3001";
 
-export const loginUser = async (user) => {//el user se lo he metido cuando he implementado la funcion en la otra funcion logMe
-    let res = await axios.post(dataBase +"/auth/login", user)// Pasariamos un body, que en este caso es user.
-    return res;
+export const loginUser = async (user) => {
+  //el user se lo he metido cuando he implementado la funcion en la otra funcion logMe
+  let res = await axios.post(dataBase + "/auth/login", user); // Pasariamos un body, que en este caso es user.
+  return res;
 };
 
 export const registerUser = async (user) => {
-    let res = await axios.post(dataBase +"/auth/register", user)
-    return res;
+  let res = await axios.post(dataBase + "/auth/register", user);
+  return res;
 };
-
 
 //Ultimas añadidas(HAY QUE CAMBIAR LOS ENDPOINTS POR LOS DE NUESTRA BASEDEDATOS)
 export const bringMovies = async () => {
+  let res = await movieApi.get(
+    "/movie/top_rated?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&page=1"
+  );
 
-    let res = await axios.get("https://api.themoviedb.org/3/movie/top_rated?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&page=1");
-
-    return res.data.results;
+  return res.data.results;
 };
 
 export const searchMovies = async (criteria) => {
+  const res = await movieApi.get(
+    `/movie?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&query=${criteria}&page=1&include_adult=false`
+  );
 
-    let res = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&query=${criteria}&page=1&include_adult=false`);
-
-    return res.data.results;
-}
+  return res.data.results;
+};
 
 export const bringSeries = async () => {
+  let res = await movieApi.get(
+    "/movie/top_rated?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&page=4"
+  );
 
-    let res = await axios.get("https://api.themoviedb.org/3/movie/top_rated?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&page=4");
-
-    return res.data.results;
+  return res.data.results;
 };
 
 export const searchSeries = async (criteria) => {
+  let res = await axios.get(
+    `/search/movie?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&query=${criteria}&page=4&include_adult=false`
+  );
 
-    let res = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&query=${criteria}&page=4&include_adult=false`);
-
-    return res.data.results;
-}
-
-
-
-
+  return res.data.results;
+};
 
 // export const loginUser = async (user) => {
 //     try {
