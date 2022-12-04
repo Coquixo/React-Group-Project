419159512455a40383e6abfb07f238850be3c246
Alex Lopez <coquixo98@gmail.com>
2022-12-04 19:20:49 +0100

    admin settings appears only when admin is logen

--- src/containers/User/UserSettings/userSettings.jsx
+++ src/containers/User/UserSettings/userSettings.jsx
@@ -36,7 +36,6 @@ const UserSettings = () => {
     password2Error: "",
   });
 
-
   const [notEmail, setNotEmail] = useState("");
 
   console.log(userReduxCredentials)
@@ -123,176 +122,311 @@ const UserSettings = () => {
   const togglePassword = () => {
     setPasswordShown(!passwordShown);
   };
-  return (
-    <div className="settingsViewDesign">
+
+  if (userReduxCredentials?.credentials?.rolIdRol === "admin") {
+
+    return (
+      <div className="settingsViewDesign">
 
 
-      <div className="settingsBoxDesign">
+        <div className="settingsBoxDesign">
 
 
-        <h1 className="updateTittleDesign">Update your credentials</h1>
-        <form onSubmit={handleSubmit} className="formSquare2">
-          <p>NAME</p>
-          <input
-            type="text"
-            name="name"
-            value={user.name}
-            className="updateInputs"
-            placeholder="Name"
-            onChange={inputHandler}
-            onInput={(e) => errorHandler(e.target.name, e.target.value, "text")}
-          />
-          <p>Surname:</p>
-          <div className="errorInput">{userError.nameError}</div>
-          <input
-            type="text"
-            name="surname"
-            value={user.surname}
-            className="updateInputs"
-            placeholder="Surname"
-            onChange={inputHandler}
-            onInput={(e) => errorHandler(e.target.name, e.target.value, "text")}
-          />
-          <div className="errorInput">{userError.surnameError}</div>
-          <p>Mail:</p>
-          <input
-            type="text"
-            name="email"
-            value={user.email}
-            className="updateInputs"
-            placeholder="Email"
-            onChange={inputHandler}
-            onInput={(e) =>
-              errorHandler(e.target.name, e.target.value, "email")
-            }
-          />
-          <div className="errorInput">{userError.emailError}</div>
-          <p>Phone:</p>
-          <input
-            type="text"
-            name="phone"
-            className="updateInputs"
-            value={user.phone}
-            placeholder="Phone Number"
-            onChange={inputHandler}
-            onInput={(e) =>
-              errorHandler(e.target.name, e.target.value, "phone")
-            }
-          />
-          <div className="errorInput">{userError.phoneError}</div>
-          <p>Adress:</p>
-          <input
-            type="text"
-            name="adress"
-            className="updateInputs"
-            value={user.address}
-            placeholder="Adress"
-            onChange={inputHandler}
-            onInput={(e) =>
-              errorHandler(e.target.name, e.target.value, "adress")
-            }
-          />
-          <div className="errorInput">{userError.adressError}</div>
-          <p>Password:</p>
-          <div className="updateInputs inputContainer">
+          <h1 className="updateTittleDesign">Update your credentials</h1>
+          <form onSubmit={handleSubmit} className="formSquare2">
+            <p>NAME</p>
             <input
-              className="inputDesign passwordInput"
-              type={passwordShown ? "text" : "password"}
-              name="password"
-              value={user.password}
-              placeholder="Password"
+              type="text"
+              name="name"
+              value={user.name}
+              className="updateInputs"
+              placeholder="Name"
+              onChange={inputHandler}
+              onInput={(e) => errorHandler(e.target.name, e.target.value, "text")}
+            />
+            <p>Surname:</p>
+            <div className="errorInput">{userError.nameError}</div>
+            <input
+              type="text"
+              name="surname"
+              value={user.surname}
+              className="updateInputs"
+              placeholder="Surname"
+              onChange={inputHandler}
+              onInput={(e) => errorHandler(e.target.name, e.target.value, "text")}
+            />
+            <div className="errorInput">{userError.surnameError}</div>
+            <p>Mail:</p>
+            <input
+              type="text"
+              name="email"
+              value={user.email}
+              className="updateInputs"
+              placeholder="Email"
+              onChange={inputHandler}
+              onInput={(e) =>
+                errorHandler(e.target.name, e.target.value, "email")
+              }
+            />
+            <div className="errorInput">{userError.emailError}</div>
+            <p>Phone:</p>
+            <input
+              type="text"
+              name="phone"
+              className="updateInputs"
+              value={user.phone}
+              placeholder="Phone Number"
+              onChange={inputHandler}
+              onInput={(e) =>
+                errorHandler(e.target.name, e.target.value, "phone")
+              }
+            />
+            <div className="errorInput">{userError.phoneError}</div>
+            <p>Adress:</p>
+            <input
+              type="text"
+              name="adress"
+              className="updateInputs"
+              value={user.address}
+              placeholder="Adress"
+              onChange={inputHandler}
+              onInput={(e) =>
+                errorHandler(e.target.name, e.target.value, "adress")
+              }
+            />
+            <div className="errorInput">{userError.adressError}</div>
+            <p>Password:</p>
+            <div className="updateInputs inputContainer">
+              <input
+                className="inputDesign passwordInput"
+                type={passwordShown ? "text" : "password"}
+                name="password"
+                value={user.password}
+                placeholder="Password"
+                onChange={inputHandler}
+                onInput={(e) =>
+                  errorHandler(e.target.name, e.target.value, "password")
+                }
+              />
+              {passwordShown ? (
+                <EyeSlashIcon classes="eyeIcon" onClick={togglePassword} />
+              ) : (
+                <EyeIcon classes="eyeIcon" onClick={togglePassword} />
+              )}
+            </div>
+            <div className="errorInput">{userError.passwordError}</div>
+            <p>Repeat Your Password:</p>
+            <input
+              type="password"
+              name="password2"
+              className="updateInputs"
+              value={user.password2}
+              placeholder="Repeat your password"
               onChange={inputHandler}
               onInput={(e) =>
                 errorHandler(e.target.name, e.target.value, "password")
               }
             />
-            {passwordShown ? (
-              <EyeSlashIcon classes="eyeIcon" onClick={togglePassword} />
-            ) : (
-              <EyeIcon classes="eyeIcon" onClick={togglePassword} />
-            )}
-          </div>
-          <div className="errorInput">{userError.passwordError}</div>
-          <p>Repeat Your Password:</p>
-          <input
-            type="password"
-            name="password2"
-            className="updateInputs"
-            value={user.password2}
-            placeholder="Repeat your password"
-            onChange={inputHandler}
-            onInput={(e) =>
-              errorHandler(e.target.name, e.target.value, "password")
-            }
-          />
-          <div className="errorInput">{userError.password2Error}</div>
-          <div className="adviseDesign">
+            <div className="errorInput">{userError.password2Error}</div>
+            <div className="adviseDesign">
+              <input
+                type="checkbox"
+                defaultChecked={acceptedTerms}
+                onChange={() => setAcceptedTerms(!acceptedTerms)}
+              />
+
+              <p>
+
+                I'm sure that i want to change that
+              </p>
+            </div>
+            <br></br>
             <input
-              type="checkbox"
-              defaultChecked={acceptedTerms}
-              onChange={() => setAcceptedTerms(!acceptedTerms)}
+              type="submit"
+              value="Update Now!"
+              className="submitButton"
+              disabled={disabled}
             />
-
-            <p>
-
-              I'm sure that i want to change that
-            </p>
-          </div>
-          <br></br>
-          <input
-            type="submit"
-            value="Update Now!"
-            className="submitButton"
-            disabled={disabled}
-          />
-        </form>
-      </div >
-      <div className="settingsBoxDesign">
-
-        <h1 className="updateTittleDesign">Admin Settings</h1>
-        <div className="formSquare2">
-          <form action="" className="eraseBox">
-
-            <input type="text" name="notEmail" className="eraseInput" placeholder="user Email" onChange={inputEraseHandler} />
-            <input type="button" className="eraseButton" value="Erase user" onClick={handleEraseSubmit} />
           </form>
-          <br />
-          User's List
+        </div >
+        <div className="settingsBoxDesign">
 
-          {users.map((user) => {
-            return (
-              <div className="usersBoxDesign">
-                Id: {user.id_user}
-                <br />
-                User: {user.name}{user.surname}
-                <br />
-                Email: {user.email}
-                <br />
-              </div>
-            )
-          })}
 
-          Orders List
+          <h1 className="updateTittleDesign">Admin Settings</h1>
+          <div className="formSquare2">
+            <form action="" className="eraseBox">
 
-          {ordersUsers.map((order) => {
-            return (
-              <div className="usersBoxDesign">
-                Order Id: {order.id_order}
-                <br />
-                User's Order Id {order.UserIdUser}
-                <br />
-                Article's Order Id: {order.ArticleIdArticle}
-                <br />
-              </div>
-            )
-          })}
+              <input type="text" name="notEmail" className="eraseInput" placeholder="user Email" onChange={inputEraseHandler} />
+              <input type="button" className="eraseButton" value="Erase user" onClick={handleEraseSubmit} />
+            </form>
+            <br />
+            User's List
 
+            {users.map((user) => {
+              return (
+                <div className="usersBoxDesign">
+                  Id: {user.id_user}
+                  <br />
+                  User: {user.name}{user.surname}
+                  <br />
+                  Email: {user.email}
+                  <br />
+                </div>
+              )
+            })}
+
+            Orders List
+
+            {ordersUsers.map((order) => {
+              return (
+                <div className="usersBoxDesign">
+                  Order Id: {order.id_order}
+                  <br />
+                  User's Order Id {order.UserIdUser}
+                  <br />
+                  Article's Order Id: {order.ArticleIdArticle}
+                  <br />
+                </div>
+              )
+            })}
+
+          </div>
         </div>
       </div>
-    </div>
 
 
-  );
+
+    );
+
+  }
+  else {
+    return (
+      <div className="settingsViewDesign">
+
+
+        <div className="settingsBoxDesign">
+
+
+          <h1 className="updateTittleDesign">Update your credentials</h1>
+          <form onSubmit={handleSubmit} className="formSquare2">
+            <p>NAME</p>
+            <input
+              type="text"
+              name="name"
+              value={user.name}
+              className="updateInputs"
+              placeholder="Name"
+              onChange={inputHandler}
+              onInput={(e) => errorHandler(e.target.name, e.target.value, "text")}
+            />
+            <p>Surname:</p>
+            <div className="errorInput">{userError.nameError}</div>
+            <input
+              type="text"
+              name="surname"
+              value={user.surname}
+              className="updateInputs"
+              placeholder="Surname"
+              onChange={inputHandler}
+              onInput={(e) => errorHandler(e.target.name, e.target.value, "text")}
+            />
+            <div className="errorInput">{userError.surnameError}</div>
+            <p>Mail:</p>
+            <input
+              type="text"
+              name="email"
+              value={user.email}
+              className="updateInputs"
+              placeholder="Email"
+              onChange={inputHandler}
+              onInput={(e) =>
+                errorHandler(e.target.name, e.target.value, "email")
+              }
+            />
+            <div className="errorInput">{userError.emailError}</div>
+            <p>Phone:</p>
+            <input
+              type="text"
+              name="phone"
+              className="updateInputs"
+              value={user.phone}
+              placeholder="Phone Number"
+              onChange={inputHandler}
+              onInput={(e) =>
+                errorHandler(e.target.name, e.target.value, "phone")
+              }
+            />
+            <div className="errorInput">{userError.phoneError}</div>
+            <p>Adress:</p>
+            <input
+              type="text"
+              name="adress"
+              className="updateInputs"
+              value={user.address}
+              placeholder="Adress"
+              onChange={inputHandler}
+              onInput={(e) =>
+                errorHandler(e.target.name, e.target.value, "adress")
+              }
+            />
+            <div className="errorInput">{userError.adressError}</div>
+            <p>Password:</p>
+            <div className="updateInputs inputContainer">
+              <input
+                className="inputDesign passwordInput"
+                type={passwordShown ? "text" : "password"}
+                name="password"
+                value={user.password}
+                placeholder="Password"
+                onChange={inputHandler}
+                onInput={(e) =>
+                  errorHandler(e.target.name, e.target.value, "password")
+                }
+              />
+              {passwordShown ? (
+                <EyeSlashIcon classes="eyeIcon" onClick={togglePassword} />
+              ) : (
+                <EyeIcon classes="eyeIcon" onClick={togglePassword} />
+              )}
+            </div>
+            <div className="errorInput">{userError.passwordError}</div>
+            <p>Repeat Your Password:</p>
+            <input
+              type="password"
+              name="password2"
+              className="updateInputs"
+              value={user.password2}
+              placeholder="Repeat your password"
+              onChange={inputHandler}
+              onInput={(e) =>
+                errorHandler(e.target.name, e.target.value, "password")
+              }
+            />
+            <div className="errorInput">{userError.password2Error}</div>
+            <div className="adviseDesign">
+              <input
+                type="checkbox"
+                defaultChecked={acceptedTerms}
+                onChange={() => setAcceptedTerms(!acceptedTerms)}
+              />
+
+              <p>
+
+                I'm sure that i want to change that
+              </p>
+            </div>
+            <br></br>
+            <input
+              type="submit"
+              value="Update Now!"
+              className="submitButton"
+              disabled={disabled}
+            />
+          </form>
+        </div >
+      </div>
+    )
+  }
 };
 
 export default UserSettings;
